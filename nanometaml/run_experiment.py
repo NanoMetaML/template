2ee22005fabdaa77a12c8cace8acc9a8265de3b8
Blake Wilson <wilso692@purdue.edu>
2023-04-24 19:42:02 -0400

    quick

--- nanometaml/run_experiment.py
+++ nanometaml/run_experiment.py
@@ -2,20 +2,30 @@
 import hydra
 from omegaconf import DictConfig
 import nanomodules
-
+import logging
 
 from clearml import Task
 import torch
 
 torch.set_float32_matmul_precision("medium")
 
-def run_experiment(cfg: DictConfig) -> None:
+
+def run_experiment(cfg: DictConfig, log_task=True) -> None:
     "Running experiment"
+    if log_task:
+        task = Task.init(project_name=cfg.project_name,
+                         task_name=cfg.task_name)
 
-    task = Task.init(project_name=cfg.project_name, task_name=cfg.task_name)
+    logging.debug(cfg)
+    cfg = nanomodules.configToDict(cfg)
+    loaded_dict = nanomodules.configWalkBuild(cfg)
+    logging.debug("loaded_dict: ")
+    logging.debug(loaded_dict)
 
-    trainer = nanomodules.loadModuleFromConfig(cfg.trainer)
-    dataset = nanomodules.loadModuleFromConfig(cfg.dataset)
-    model = nanomodules.loadModuleFromConfig(cfg.model)
-
-    trainer.fit(model, dataset["train"], dataset["valid"])
+    logging.basicConfig(filename='app.log',
+                        filemode='w', level=logging.DEBUG)
+    print(loaded_dict['model'])
+    print(loaded_dict['model'].model.parameters())
+#    logging.debug("Model: ", loaded_dict['model'])
+    loaded_dict['trainer'].fit(
+        loaded_dict['model'], loaded_dict['datasets']['train'], loaded_dict['datasets']['valid'])
